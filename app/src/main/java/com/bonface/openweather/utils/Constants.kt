7462b94d5b29e1af402491bfa436290fda29c549
Bonface Otieno <bonfaceotieno@Bonfaces-MacBook-Pro.local>
2023-11-19 13:45:27 +0300

    minor updates

--- app/src/main/java/com/bonface/openweather/utils/Constants.kt
+++ app/src/main/java/com/bonface/openweather/utils/Constants.kt
@@ -3,4 +3,5 @@ package com.bonface.openweather.utils
 object Constants {
     const val UNITS = "metric"
     const val EXCLUDE = "current,minutely,hourly,alerts"
+    const val TASK_AWAIT = 120L
 }
